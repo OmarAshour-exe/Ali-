3b52a4a09e32f90036fca7d9de82242cb66fda89
Omar Ashour <56017327+OmarAshour-exe@users.noreply.github.com>
2020-01-14 21:06:39 +0100

    last

--- ali-site/myscript.js
+++ ali-site/myscript.js
@@ -1,17 +1,59 @@
-document.addEventListener("DOMContentLoaded",
-	function (event) {
-		document.querySelector("button")
-		.addEventListener("click", function () {
-			$ajaxUtils
-			.sendGetRequest("name.txt", 
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
-				function (request) {
-					var name = request.responseText;
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
 
-					document.querySelector("#content")
-					.innerHTML = "<h2>Hello " + name + "!";
+  // In Firefox and Safari, the click event doesn't retain the focus
+  // on the clicked button. Therefore, the blur event will not fire on
+  // user clicking somewhere else in the page and the blur event handler
+  // which is set up above will not be called.
+  // Refer to issue #28 in the repo.
+  // Solution: force focus on the element that the click event fired on
+  $("#navbarToggle").click(function (event) {
+    $(event.target).focus();
+  });
+});
+/
+(function (global) {
+
+var dc = {};
+
+var homeHtml = "snippets/home-snippet.html";
+
+// Convenience function for inserting innerHTML for 'select'
+var insertHtml = function (selector, html) {
+  var targetElem = document.querySelector(selector);
+  targetElem.innerHTML = html;
+};
+
+// Show loading icon inside element identified by 'selector'.
+var showLoading = function (selector) {
+  var html = "<div class='text-center'>";
+  html += "<img src='Ring.gif'></div>";
+  insertHtml(selector, html);
+};
+
+// On page load (before images or CSS)
+document.addEventListener("DOMContentLoaded", function (event) {
+
+// On first load, show home view
+showLoading("#main-content");
+$ajaxUtils.sendGetRequest(
+  homeHtml,
+  function (responseText) {
+    document.querySelector("#main-content")
+      .innerHTML = responseText;
+  },
+  false);
+});
 
 
-				});
-		});
-	});
+global.$dc = dc;
+
+})(window);
+/
